ac0a869cf70c37e223779c617b457672a4a0fdf3
Diamons <romain.hure@epitech.eu>
2022-12-02 22:43:54 +0100

    STYLE(<selection_sort.c>): missing space at type declaration.

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -1,4 +1,4 @@
-def swap(int_array: list[int], a: int, b:int) -> None:
+def swap(int_array: list[int], a: int, b: int) -> None:
 	tmp = int_array[a]
 	int_array[a] = int_array[b]
 	int_array[b] = tmp
