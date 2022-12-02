60500135b2b70a5e8fd67233799a9e5579ca2448
Diamons <romain.hure@epitech.eu>
2022-12-03 00:37:06 +0100

    FEAT(<pigeonhole_sort.c>): add pigeonhole sort.

--- sort/__init__.py
+++ sort/__init__.py
@@ -2,3 +2,4 @@ from .bubble_sort import bubble_sort
 from .selection_sort import selection_sort
 from .insertion_sort import insertion_sort
 from .merge_sort import merge_sort
+from .pigeonhole_sort import pigeonhole_sort
---
+++ sort/pigeonhole_sort.py
@@ -0,0 +1,12 @@
+def pigeonhole_sort(int_array: list[int]) -> None:
+	min_val = min(int_array)
+	index_array = [None] * (max(int_array) - min_val + 1)
+
+	for i in range(len(int_array)):
+		index_array[int_array[i] - min_val] = int_array[i]
+
+	i = 0
+	for j in range(len(index_array)):
+		if index_array[j] is not None:
+			int_array[i] = index_array[j]
+			i += 1
